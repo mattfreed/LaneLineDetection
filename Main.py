bb67df8b8628efef596d21e5d8ed69a9c3656150
mattfreed <mwfreed@wpi.edu>
2019-06-24 12:20:53 -0400

    Detecting lines per frame

--- Main.py
+++ Main.py
@@ -19,24 +19,64 @@ def regionOfInterest(image):
 def display_Lines(image, lines):
     line_image = np.zeros_like(image)
     if lines is not None:
-        for line in lines:
-            x1, y1, x2, y2 = line.reshape(4)
+        for x1, y1, x2, y2 in lines:
             cv2.line(line_image, (x1, y1), (x2, y2), (255, 0, 0), 10)
     return line_image
 
+def averageSlopedIntercept(image, liens):
+    leftFit = []
+    rightFit = []
+    for line in lines:
+        x1, y1, x2, y2 = line.reshape(4)
+        parameters = np.polyfit((x1, x2), (y1, y2), 1)
+        slope = parameters[0]
+        intercept = parameters[1]
+        if slope <0:
+            leftFit.append((slope,intercept))
+        else:
+            rightFit.append((slope,intercept))
+    leftFitAverage = np.average(leftFit,axis = 0)
+    rightFitAverage = np.average(rightFit, axis = 0)
+    leftLine = makeCoordinates(image, leftFitAverage)
+    rightLine = makeCoordinates(image, rightFitAverage)
+    return np.array([leftLine, rightLine])
 
-image = cv2.imread('Picture/test_image.jpg')
-lane_image = np.copy(image)
-lane_image2 = np.copy(image)
-cannyImage = cannyDisplay(lane_image)
-croppedImage = regionOfInterest(cannyImage)
-lines = cv2.HoughLinesP(croppedImage, 2, np.pi/180, 100, np.array([]), minLineLength=40, maxLineGap=5)
-line_image = display_Lines(lane_image2, lines)
-comboImage = cv2.addWeighted(lane_image2, 0.8, line_image, 1, 1)
+def makeCoordinates(image, lineParameters):
+    slope, intercept = lineParameters
+    y1 = image.shape[0]
+    y2 = int(y1*(3/5))
+    x1 = int((y1-intercept)/slope)
+    x2 = int((y2 - intercept) / slope)
+    return np.array([x1, y1, x2,y2])
 
-cv2.imshow('result1', image)
-cv2.imshow('result2', croppedImage)
-cv2.imshow('result3', line_image)
-cv2.imshow('result4', comboImage)
-cv2.waitKey(0)
 
+
+# image = cv2.imread('Picture/test_image.jpg')
+# lane_image = np.copy(image)
+# cannyImage = cannyDisplay(lane_image)
+# croppedImage = regionOfInterest(cannyImage)
+# lines = cv2.HoughLinesP(croppedImage, 2, np.pi/180, 100, np.array([]), minLineLength=40, maxLineGap=5)
+# avgLines = averageSlopedIntercept(lane_image,lines)
+# line_image = display_Lines(lane_image, avgLines)
+# comboImage = cv2.addWeighted(lane_image, 0.8, line_image, 1, 1)
+#
+# cv2.imshow('result1', image)
+# cv2.imshow('result2', croppedImage)
+# cv2.imshow('result3', line_image)
+# cv2.imshow('result4', comboImage)
+# # cv2.waitKey(0)
+
+video = cv2.VideoCapture("Video/test2.mp4")
+while(video.isOpened()):
+    _, frame = video.read()
+    cannyImage = cannyDisplay(frame)
+    croppedImage = regionOfInterest(cannyImage)
+    lines = cv2.HoughLinesP(croppedImage, 2, np.pi / 180, 100, np.array([]), minLineLength=40, maxLineGap=5)
+    avgLines = averageSlopedIntercept(frame, lines)
+    line_image = display_Lines(frame, avgLines)
+    comboImage = cv2.addWeighted(frame, 0.8, line_image, 1, 1)
+    cv2.imshow('video', comboImage)
+    if cv2.waitKey(1) == ord('q'):
+        break
+video.release()
+cv2.destroyAllWindows()
